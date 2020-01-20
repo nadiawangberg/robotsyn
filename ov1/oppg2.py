11d5702a0e9d0e5fb00f7e5ea846fb9645dd1135
nadiawangberg <nadiagw@stud.ntnu.no>
2020-01-20 15:49:29 +0100

    øving finished :D

--- ov1/oppg2.py
+++ ov1/oppg2.py
@@ -3,20 +3,11 @@ import matplotlib.image as mpimg
 import math as math
 import numpy as np
 
-
 #Platform to camera matrix
 T_p_to_c = [[0.894372, -0.447712, 0.0127064, -0.25861],
 		    [-0.0929288, -0.213413, -0.972924, 0.116584],
 		    [0.438049, 0.868713, -0.232355, 0.791487],
 		    [0, 0, 0, 1]]
-
-
-screw0 = np.array([[0], [0], [0], [1]])
-screw1 = np.array([[0.1145], [0], [0], [1]])
-screw2 = np.array([[0], [0.1145], [0], [1]])
-screw3 = np.array([[0.1145], [0.1145], [0], [1]])
-
-
 
 def Translate(t_x, t_y, t_z):
     return np.array(
@@ -60,7 +51,7 @@ def R_z(theta):
 
 def cam_to_pxl(X,Y,Z): # task 1d
 	#camera intrinsic (heli_intrinsics)
-	#everything should be given in meters
+	#X,Y,Z should be given in meters
 	fx = 1075.47
 	fy = 1077.22
 	cx = 621.01
@@ -75,28 +66,37 @@ def point(x,y,z):
     return np.array([[x], [y], [z], [1]])
 
 def draw_line(a, b, **args):
-    plt.plot([a[0], b[0]], [a[1], b[1]], **args, linewidth=4)
+    plt.plot([a[0], b[0]], [a[1], b[1]], **args, linewidth=3.5)
 
 def draw_frame(T):
+	#From T to camera
     origin = T@point(0,0,0)
     o1,o2 = cam_to_pxl(origin[0], origin[1], origin[2])
 
-    x_axis = T@point(0.1,0,0) #red
-    y_axis = T@point(0,0.1,0) #green
-    z_axis = T@point(0,0,0.1) #blue
+    x_axis = T@point(0.05,0,0) #red
+    y_axis = T@point(0,0.05,0) #green
+    z_axis = T@point(0,0,0.05) #blue
 
+    #From cam to pixel coordinates
     x1,x2 = cam_to_pxl(x_axis[0], x_axis[1], x_axis[2])
     y1,y2 = cam_to_pxl(y_axis[0], y_axis[1], y_axis[2])
     z1,z2 = cam_to_pxl(z_axis[0], z_axis[1], z_axis[2])
 
+    #Draw x,y,z axis
     draw_line([o1,o2], [x1,x2], color='red')
     draw_line([o1,o2], [y1,y2], color='green')
     draw_line([o1,o2], [z1,z2], color='blue')
 
     #plt.text(o1- 30,o2 - 5, label)
 
+# oppg 2a
 img=mpimg.imread('quanser.jpg')
 imgplot = plt.imshow(img)
+
+screw0 = np.array([[0], [0], [0], [1]])
+screw1 = np.array([[0.1145], [0], [0], [1]])
+screw2 = np.array([[0], [0.1145], [0], [1]])
+screw3 = np.array([[0.1145], [0.1145], [0], [1]])
 
 screw0_cam = T_p_to_c @ screw0
 x,y = cam_to_pxl(screw0_cam[0], screw0_cam[1], screw0_cam[2])
@@ -114,11 +114,26 @@ screw3_cam = T_p_to_c @ screw3
 x,y = cam_to_pxl(screw3_cam[0], screw3_cam[1], screw3_cam[2])
 plt.plot(x,y, 'wo')
 
+#oppg 2b
 draw_frame(T_p_to_c)
 
 #oppg 3c
-phi = 10
-yaw_motion = T_p_to_c @ Translate(0.0573, 0.0573, 0) @ R_z(phi)
-draw_frame(yaw_motion)
+psi = 11.77 #Yaw
+T_base_to_plat = T_p_to_c @ Translate(0.0573, 0.0573, 0) @ R_z(psi)
+draw_frame(T_base_to_plat) #trans to cam to pix
+
+#oppg 3d
+theta = 28.87 #Pitch
+T_hinge_to_base = T_base_to_plat @ Translate(0, 0, 0.325) @ R_y(theta)
+draw_frame(T_hinge_to_base)
+
+#oppg 3d
+T_arm_to_hinge = T_hinge_to_base @ Translate(0, 0, -0.0552)
+draw_frame(T_arm_to_hinge)
+
+#oppg 3e
+phi = -0.5
+T_rotors_to_arm = T_arm_to_hinge @ Translate(0.653, 0, -0.0312)
+draw_frame(T_rotors_to_arm)
 
 plt.show()
