136ff1ee6a742a41adcd223afc0bcac849e34270
nadiawangberg <nadiagw@stud.ntnu.no>
2020-01-20 15:03:04 +0100

    finished task 1d

--- ov1/oppg1.py
+++ ov1/oppg1.py
@@ -76,6 +76,32 @@ def plot_2d_box(): #task 1b
     ax_2 = plt.axis([0,640, 0, 480])
     plt.plot(box_cp[:,0], box_cp[:,1], 'ro')
 
+
+def point(x,y,z):
+    return np.array([[x], [y], [z], [1]])
+
+def draw_line(a, b, **args):
+    plt.plot([a[0], b[0]], [a[1], b[1]], **args, linewidth=2)
+
+def draw_frame(T, label):
+    origin = T@point(0,0,0)
+    o1,o2 = cam_to_pxl(origin[0], origin[1], origin[2])
+
+    x_axis = T@point(1,0,0) #red
+    y_axis = T@point(0,1,0) #green
+    z_axis = T@point(0,0,1) #blue
+
+    x1,x2 = cam_to_pxl(x_axis[0], x_axis[1], x_axis[2])
+    y1,y2 = cam_to_pxl(y_axis[0], y_axis[1], y_axis[2])
+    z1,z2 = cam_to_pxl(z_axis[0], z_axis[1], z_axis[2])
+
+    draw_line([o1,o2], [x1,x2], color='red')
+    draw_line([o1,o2], [y1,y2], color='green')
+    draw_line([o1,o2], [z1,z2], color='blue')
+
+    plt.text(o1- 30,o2 - 5, label)
+
+
 def transform_box(): #task 1d
     #copy of global box variable
     box_cp = box.copy()
@@ -87,12 +113,18 @@ def transform_box(): #task 1d
         # cam starts within the center of the box
         # the box is translated 5 away, putting the box in the correct pos in respect to the camera
         # the box is translated 1 away, then rotated in respect to ITS axis
+
         box_cp[p][0], box_cp[p][1] = cam_to_pxl(box_cp[p][0],box_cp[p][1],box_cp[p][2]) # coordinates translated +5 to turn into cam coords. 
     
+
     #plot box 2d
     fig_2 = plt.figure(3)
     ax_2 = plt.axis([0,640, 480, 0])
+
+    #Draw transform
+    draw_frame(T_o_to_c, "obj")
     plt.plot(box_cp[:,0], box_cp[:,1], 'bo')
+
 
 #load box
 box = np.loadtxt("box.txt")
